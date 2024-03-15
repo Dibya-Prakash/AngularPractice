3fcbaad854c6980ecd6c740513746a81f503cd9f
Dibya-Prakash <33830594+Dibya-Prakash@users.noreply.github.com>
2024-03-15 13:13:00 +0530

    Adding the UI project
    This folder contains the project files that are used for UI using Angular JS framework

---
+++ Employee.UI/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './components/employees/employee-list/employee-list.component';
+import { AddEmployeeComponent } from './components/employees/add-employee/add-employee.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component:EmployeeListComponent
+  },
+  {
+    path:'employees',
+    component:EmployeeListComponent
+  },
+  {
+    path:'employees/add',
+    component:AddEmployeeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
