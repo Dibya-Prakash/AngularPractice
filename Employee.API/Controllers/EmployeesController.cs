815c91b7f771950f66e3ee21bfced791cc636154
Dibya Prakash Paul <dibyappaul@gmail.com>
2024-03-17 15:56:35 +0530

    Update Functionality added

--- Employee.API/Controllers/EmployeesController.cs
+++ Employee.API/Controllers/EmployeesController.cs
@@ -30,5 +30,41 @@ namespace Employee.API.Controllers
             await _employeeDbContext.SaveChangesAsync();
             return Ok(employeeRequest);
         }
+
+        [HttpGet]
+        [Route("edit/{id:Guid}")]
+        public async Task<IActionResult> GetEmployee([FromRoute] Guid id)
+        {
+            var employee = await _employeeDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        [Route("edit/{id:Guid}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] Guid id, Employees updateEmployeeRequest)
+        {
+            var employee = await _employeeDbContext.Employees.FindAsync(id);
+
+            if(employee == null)
+            {
+                return NotFound();
+            }
+            employee.Name = updateEmployeeRequest.Name;
+            employee.Email = updateEmployeeRequest.Email;
+            employee.Phone = updateEmployeeRequest.Phone;
+            employee.Salary = updateEmployeeRequest.Salary;
+            employee.Department= updateEmployeeRequest.Department;
+
+            await _employeeDbContext.SaveChangesAsync();
+
+            return Ok(employee);
+
+        }
+
     }
 }
